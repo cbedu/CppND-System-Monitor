dad8e4ac66bd5cc7a35e0cb75e945be443f1aa3c
cbedu <91734783+cbedu@users.noreply.github.com>
2022-01-19 07:33:51 +0000

    WIP Command revmove maybe-unused

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -154,7 +154,7 @@ int LinuxParser::RunningProcesses()
 
 // TESTING : Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]])
+string LinuxParser::Command(int pid)
 {
   // easy: /proc/[pid]/cmdline
   string line;
