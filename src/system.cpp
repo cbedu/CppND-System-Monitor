c9e1eb3d8fd4bd745ae5642e218a4faaf9c4c732
cbedu <91734783+cbedu@users.noreply.github.com>
2022-01-20 13:10:23 +0000

    Debug
    Arbitrarily crashing.
    Anywhere from 7seconds to many minutes.

--- src/system.cpp
+++ src/system.cpp
@@ -20,7 +20,7 @@ You need to properly format the uptime. Refer to the comments mentioned in forma
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// UPDATE SORT : Return a container composed of the system's processes
+// FIND LEAKING ITERATOR : Return a container composed of the system's processes
 vector<Process>& System::Processes()
 {
     // Get list of process ids
