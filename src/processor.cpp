c05a110a5d927814ad81526133cb678be6d85928
cbedu <91734783+cbedu@users.noreply.github.com>
2022-02-09 09:21:49 +0000

    Move Jiffy calc out from processorCpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -4,23 +4,5 @@
 // DONE : Return the aggregate CPU utilization
 float Processor::Utilization()
 {
-    // grab from functions once
-    long tempActive = LinuxParser::ActiveJiffiesDelta();
-    //long tempIdle = LinuxParser::IdleJiffiesDelta();
-    long tempTotal = LinuxParser::JiffiesDelta();
-
-    //return 1.00f;
-    return (float)tempActive / (float)tempTotal;
-
-    // compare against our last run
-    //long idleJiffiesDelta = tempIdle - previousIdleJiffies_;
-    long totalJiffiesDelta = tempTotal - previousTotalJiffies_;
-    long activeJiffiesDelta = tempActive - previousActiveJiffies_;
-
-    // store latest values for next comparison
-    //previousIdleJiffies_ = tempIdle;
-    previousTotalJiffies_ = tempTotal;
-    previousActiveJiffies_ = tempActive;
-
-    return (float)activeJiffiesDelta / (float)totalJiffiesDelta;
+    return (float)LinuxParser::ActiveJiffiesDelta() / (float)LinuxParser::JiffiesDelta();
 }
